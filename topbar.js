86d9716655451c148a7d3080bd9e242484eede52
kumilullaby <202365131+kumilullaby@users.noreply.github.com>
2025-12-03 07:13:44 +0000

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -1,10 +1,10 @@
-var html = '<li><a href="index.html">Home<(a></li>';
+var html = '<a href="index.html">Home</a>';
 
 var section1 = ['HTML', 'Java', 'CSS']
 for (i = 0; i<section1.length; i++){
-	html += '<li><a href="' + section1[i] + '.html">' + section1[i] + '</a></li>'
+	html += '<a href="' + section1[i] + '.html">' + section1[i] + '</a>'
 }
 
-html += '<li><a href="https://youtu.be/dQw4w9WgXcQ">Custom Link</a></li>'
+html += '<a href="https://youtu.be/dQw4w9WgXcQ">Custom Link</a>'
 
 document.getElementById("templaterr-topbar").innerHTML = html;
