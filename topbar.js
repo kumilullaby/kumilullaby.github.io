0374c2d3d34455a5ff8520ebda15669db90bbc4d
kumilullaby <202365131+kumilullaby@users.noreply.github.com>
2025-12-03 07:53:48 +0000

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -1,9 +1,9 @@
 var html = '<a href="/">■ Home</a> ';
 
-html += '<a href="/about">■ About</a>'
-html += '<a href="/gallery">■ Gallery</a>'
-html += '<a href="/blog">■ Blog</a>'
-html += '<a href="/translations">■ Translations</a>'
-html += '<a href="/credits">■ Credits</a>'
+html += '<a href="/about">■ About</a> '
+html += '<a href="/gallery">■ Gallery</a> '
+html += '<a href="/blog">■ Blog</a> '
+html += '<a href="/translations">■ Translations</a> '
+html += '<a href="/credits">■ Credits</a> '
 
 document.getElementById("templaterr-topbar").innerHTML = html;
