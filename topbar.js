9ed89b9194dda713453363ca0ec80aff63bec482
kumilullaby <202365131+kumilullaby@users.noreply.github.com>
2025-12-03 07:24:26 +0000

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -2,7 +2,7 @@ var html = '<a href="index.html">Home</a>';
 
 var section1 = ['HTML', 'Java', 'CSS']
 for (i = 0; i<section1.length; i++){
-	html += '<a href="' + section1[i] + '.html">' + section1[i] + '</a>'
+	html += '<a href="' + section1[i] + '.html" class="WHATEVERMANIJUSTWANTANAVIGATIONBAR">' + section1[i] + '</a>'
 }
 
 html += '<a href="https://youtu.be/dQw4w9WgXcQ">Custom Link</a>'
