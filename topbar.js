bd16c7aed984872b83f16420626f684564e05986
kumilullaby <202365131+kumilullaby@users.noreply.github.com>
2025-12-05 02:12:50 +0000

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -1,9 +1,23 @@
-var html = '<a href="/">■ Home</a> ';
+html += '<nav>'
+html += '<ul>'
 
-html += '<a href="/about">■ About</a> '
-html += '<a href="/gallery">■ Gallery</a> '
-html += '<a href="/blog">■ Blog</a> '
-html += '<a href="/translations">■ Translations</a> '
-html += '<a href="/credits">■ Credits</a> '
+var html = '<li><a href="/">■ Home</a></li> ';
+html += '<li><a href="/about">■ About</a></li> '
+html += '<li><a href="/gallery">■ Gallery</a></li> '
+html += '<li><a href="/blog">■ Blog</a></li> '
+html += '<li><a href="/translations">■ Translations</a></li> '
+html += '<li><a href="/credits">■ Credits</a></li> '
+html += '<li>'
+html += '<ul>'
+html += '<strong>Submenu (hover)</strong>'
+html += '<li><a href="/writing">■ Writing</a></li>'
+html += '<li><a href="https://marshmallow-qa.com/6d379n294ez7eal" target="_blank">■ Ask Box</a></li>'
+html += '<li><a href="/philosophy">■ Philosophy</a></li>'
+
+
+html += '</ul>'
+html += '</li>'
+html += '</ul>'
+html += '</nav>'
 
 document.getElementById("templaterr-topbar").innerHTML = html;
