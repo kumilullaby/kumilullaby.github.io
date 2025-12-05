45d7f5f1b42331f062c7e5bb6e8738755275eacd
kumilullaby <202365131+kumilullaby@users.noreply.github.com>
2025-12-05 01:59:45 +0000

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -1,28 +1,9 @@
-const js_thml = () => {
-	let code = "";
-	code += "	\t\t<nav>\n";
-	code += "	\t\t  <ul>\n";
-	code += "	\t\t\t<li><a href=\"/\">■ Home</a></li>\n";
-	code += "	\t\t\t<li><a href=\"/about\">■ About</a></li>\n";
-	code += "	\t\t\t<li><a href=\"/gallery\">■ Gallery</a></li>\n";
-	code += "	\t\t\t<li><a href=\"/blog\">■ Blog</a></li>\n";
-	code += "	\t\t\t<li><a href=\"/translations\">■ Translations</a></li>\n";
-	code += "	\t\t\t<li><a href=\"/credits\">■ Credits</a></li>\n";
-	code += "\n";
-	code += "	\t\t\t<li>\n";
-	code += "	\t\t\t\t<strong>Submenu (hover)</strong>\n";
-	code += "	\t\t\t\t<ul>\n";
-	code += "	\t\t\t\t  <li><a href=\"/writing\">■ Writing</a></li>\n";
-	code += "	\t\t\t\t  <li><a href=\"https://marshmallow-qa.com/6d379n294ez7eal\" target=\"_blank\">■ Ask Box</a></li>\n";
-	code += "					  <li><a href=\"/philosophy\">■ Philosophy</a></li>\n";
-	code += "\n";
-	code += "	\t\t\t\t</ul>\n";
-	code += "	\t\t\t</li>\n";
-	code += "	\t\t  </ul>\n";
-	code += "	\t\t</nav>\n";
-	return code;
-}
+var html = '<a href="/">■ Home</a> ';
 
+html += '<a href="/about">■ About</a> '
+html += '<a href="/gallery">■ Gallery</a> '
+html += '<a href="/blog">■ Blog</a> '
+html += '<a href="/translations">■ Translations</a> '
+html += '<a href="/credits">■ Credits</a> '
 
-document.getElementById("js_html").innerText = js_thml();
-document.getElementById("html-text").value = js_thml();
+document.getElementById("templaterr-topbar").innerHTML = html;
