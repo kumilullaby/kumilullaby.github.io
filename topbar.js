28122113bbe0c80642b53646d3f4826918d75d15
kumilullaby <202365131+kumilullaby@users.noreply.github.com>
2025-12-05 02:00:35 +0000

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -5,5 +5,18 @@ html += '<a href="/gallery">■ Gallery</a> '
 html += '<a href="/blog">■ Blog</a> '
 html += '<a href="/translations">■ Translations</a> '
 html += '<a href="/credits">■ Credits</a> '
+html += '<li>
+	                <strong>Submenu (hover)</strong>
+	                <ul>
+	                  <li><a href="/writing">■ Writing</a></li>
+	                  <li><a href="https://marshmallow-qa.com/6d379n294ez7eal" target="_blank">■ Ask Box</a></li>
+					  <li><a href="/philosophy">■ Philosophy</a></li>
+
+
+
+	                </ul>
+	            </li>
+	          </ul>
+	        </nav>'
 
 document.getElementById("templaterr-topbar").innerHTML = html;
