e3674a4d863c11201a52b83fec1d2b8774ba7562
kumilullaby <202365131+kumilullaby@users.noreply.github.com>
2025-12-05 01:54:20 +0000

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -1,9 +1,22 @@
-var html = '<a href="/">■ Home</a> ';
-
-html += '<a href="/about">■ About</a> '
-html += '<a href="/gallery">■ Gallery</a> '
-html += '<a href="/blog">■ Blog</a> '
-html += '<a href="/translations">■ Translations</a> '
-html += '<a href="/credits">■ Credits</a> '
-
-document.getElementById("templaterr-topbar").innerHTML = html;
+<script language="JavaScript">
+<!--
+document.writeln("<nav>");
+document.writeln("          <ul>");
+document.writeln("            <li><a href=/"//">■ Home</a></li>");
+document.writeln("            <li><a href=/"/about/">■ About</a></li>");
+document.writeln("            <li><a href=/"/gallery/">■ Gallery</a></li>");
+document.writeln("            <li><a href=/"/blog/">■ Blog</a></li>");
+document.writeln("            <li><a href=/"/translations/">■ Translations</a></li>");
+document.writeln("            <li><a href=/"/credits/">■ Credits</a></li>");
+document.writeln("	            <li>");
+document.writeln("                <strong>Submenu (hover)</strong>");
+document.writeln("                <ul>");
+document.writeln("                  <li><a href=/"/writing/">■ Writing</a></li>");
+document.writeln("                  <li><a href=/"https://marshmallow-qa.com/6d379n294ez7eal/" target=/"_blank/">■ Ask Box</a></li>");
+document.writeln("				  <li><a href=/"/philosophy/">■ Philosophy</a></li>");
+document.writeln("	                </ul>");
+document.writeln("            </li>");
+document.writeln("          </ul>");
+document.writeln("        </nav>");
+//-->
+</script>
