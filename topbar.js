041d4d15f80a17237a60d24ec3f0ef6188512e3c
kumilullaby <202365131+kumilullaby@users.noreply.github.com>
2025-12-05 01:56:08 +0000

    Update topbar.js

--- topbar.js
+++ topbar.js
@@ -1,18 +1,28 @@
-document.writeln("<nav>");
-document.writeln("          <ul>");
-document.writeln("            <li><a href=/"//">■ Home</a></li>");
-document.writeln("            <li><a href=/"/about/">■ About</a></li>");
-document.writeln("            <li><a href=/"/gallery/">■ Gallery</a></li>");
-document.writeln("            <li><a href=/"/blog/">■ Blog</a></li>");
-document.writeln("            <li><a href=/"/translations/">■ Translations</a></li>");
-document.writeln("            <li><a href=/"/credits/">■ Credits</a></li>");
-document.writeln("	            <li>");
-document.writeln("                <strong>Submenu (hover)</strong>");
-document.writeln("                <ul>");
-document.writeln("                  <li><a href=/"/writing/">■ Writing</a></li>");
-document.writeln("                  <li><a href=/"https://marshmallow-qa.com/6d379n294ez7eal/" target=/"_blank/">■ Ask Box</a></li>");
-document.writeln("				  <li><a href=/"/philosophy/">■ Philosophy</a></li>");
-document.writeln("	                </ul>");
-document.writeln("            </li>");
-document.writeln("          </ul>");
-document.writeln("        </nav>");
+const js_thml = () => {
+	let code = "";
+	code += "	\t\t<nav>\n";
+	code += "	\t\t  <ul>\n";
+	code += "	\t\t\t<li><a href=\"/\">■ Home</a></li>\n";
+	code += "	\t\t\t<li><a href=\"/about\">■ About</a></li>\n";
+	code += "	\t\t\t<li><a href=\"/gallery\">■ Gallery</a></li>\n";
+	code += "	\t\t\t<li><a href=\"/blog\">■ Blog</a></li>\n";
+	code += "	\t\t\t<li><a href=\"/translations\">■ Translations</a></li>\n";
+	code += "	\t\t\t<li><a href=\"/credits\">■ Credits</a></li>\n";
+	code += "\n";
+	code += "	\t\t\t<li>\n";
+	code += "	\t\t\t\t<strong>Submenu (hover)</strong>\n";
+	code += "	\t\t\t\t<ul>\n";
+	code += "	\t\t\t\t  <li><a href=\"/writing\">■ Writing</a></li>\n";
+	code += "	\t\t\t\t  <li><a href=\"https://marshmallow-qa.com/6d379n294ez7eal\" target=\"_blank\">■ Ask Box</a></li>\n";
+	code += "					  <li><a href=\"/philosophy\">■ Philosophy</a></li>\n";
+	code += "\n";
+	code += "	\t\t\t\t</ul>\n";
+	code += "	\t\t\t</li>\n";
+	code += "	\t\t  </ul>\n";
+	code += "	\t\t</nav>\n";
+	return code;
+}
+
+
+document.getElementById("js_html").innerText = js_thml();
+document.getElementById("html-text").value = js_thml();
